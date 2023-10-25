cb4b2072d9a3bee9a59a980280f70b811f0a1918
Miguel Ramos <return.infox@gmail.com>
2023-10-25 19:34:24 -0300

    archivo que realiza la peticion

---
+++ src/services/peliculas-service.js
@@ -0,0 +1,20 @@
+const API_URL = 'https://api.themoviedb.org/3/movie/';
+const API_KEY = '0ffa38e5464cf85244a57b5a7724bd12';
+
+export const getPeliculasPopulares = async () => {
+ //* https://api.themoviedb.org/3/movie/popular?api_key=0ffa38e5464cf85244a57b5a7724bd12
+ //* https://api.themoviedb.org/3/movie/popular?apikey
+ try {
+  //* alt + 96 
+  const response = await fetch(`${API_URL}/popular?apikey=${API_KEY}`);
+  if(!response.ok) {
+    throw new Error(`Http error! Status: ${response.status}`);
+  }
+  const data = await response.json();
+  return data;
+ } catch (error) {
+   console.log('Error en la petición',  error);
+   return null;
+ }
+};
+
