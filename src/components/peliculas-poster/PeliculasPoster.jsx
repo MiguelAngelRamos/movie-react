b8239c80ed5e3b9eed371f4975ffba1a14e773e6
Miguel Ramos <return.infox@gmail.com>
2023-10-30 19:23:44 -0300

    estilos de las peliculas

--- src/components/peliculas-poster/PeliculasPoster.jsx
+++ src/components/peliculas-poster/PeliculasPoster.jsx
@@ -6,7 +6,7 @@ const PeliculasPoster = ({peliculas}) => {
   return (
     <div className="peliculas-grid">
       {peliculas.map(pelicula => (
-        <div key={pelicula.id} className="movie-card">
+        <div key={pelicula.id} className="pelicula-card">
           <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} alt={pelicula.title} />
           <h3>{pelicula.title}</h3>
         </div>
